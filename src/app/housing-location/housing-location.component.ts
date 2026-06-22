2ca540f561edf2978d59124390722754ed62a3f6
SethMilchick <martiskalenda@gmail.com>
2026-06-22 13:59:42 +0300

    Clean up

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HousingLocation} from "../housing-location";
+import { HousingLocation } from '../housing-location';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,14 +9,19 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
-      <img class="listing-photo" [src]="housingLocation.photo"
-      alt="Esterior photo of {{housingLocation.name}}">
-      <h2 class="listing-heading">{{housingLocation.name}}</h2>
-      <p class="listing-location">{{housingLocation.city}}. {{housingLocation.state}}</p>
-      <a [routerLink]="['/details', housingLocation.id]"></a>
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Esterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}. {{ housingLocation.state }}
+      </p>
+      <a [routerLink]="['/details', housingLocation.id]">Details</a>
     </section>
   `,
-  styleUrls: ['./housing-location.component.css']
+  styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
